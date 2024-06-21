c44ada2b708fa6dc5d87d3370f49c115d56b02ae
Marvel.Codes <marvellousn561@gmail.com>
2024-06-22 00:42:45 +0100

    type declaration

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,30 @@
 declare module 'next-textify' {
-
-  export function textify(input: string): string;
-
-  export interface TextifyOptions {
-    option1?: string;
-    option2?: number;
+  class Textify {
+    static capitalize(str: string): string;
+    static lowerCase(str: string): string;
+    static upperCase(str: string): string;
+    static reverse(str: string): string;
+    static trim(str: string): string;
+    static truncate(str: string, length: number): string;
+    static isEmpty(str: string): boolean;
+    static kebabCase(str: string): string;
+    static snakeCase(str: string): string;
+    static camelCase(str: string): string;
+    static pascalCase(str: string): string;
+    static titleCase(str: string): string;
+    static sentenceCase(str: string): string;
+    static slugify(str: string): string;
+    static wordCount(str: string): number;
+    static charCount(str: string): number;
+    static sentenceCount(str: string): number;
+    static paragraphCount(str: string): number;
+    static lineCount(str: string): number;
+    static paragraphize(str: string): string;
+    static sentenceize(str: string): string;
+    static tokenize(str: string): string[];
+    static wordify(str: string): string[];
+    static hashify256bits(str: string): string;
   }
 
-  export default function nextTextify(input: string, options?: TextifyOptions): string;
+  export = Textify;
 }
